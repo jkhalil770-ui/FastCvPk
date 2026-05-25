93a96752b54aae5edea251146078b66b6648896d
Muhammad Junaid Khalil <jkhalil770@gmail.com>
2026-05-25 16:05:07 +0500

    feat: final production launch - sitemap updated, SEO verified, security hardened, custom domain live

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -8,45 +8,51 @@ export async function GET() {
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
     <loc>https://fastcvpk.online/</loc>
-    <lastmod>2026-05-23</lastmod>
+    <lastmod>2026-05-25</lastmod>
     <changefreq>monthly</changefreq>
     <priority>1.0</priority>
   </url>
   <url>
     <loc>https://fastcvpk.online/templates</loc>
-    <lastmod>2026-05-23</lastmod>
+    <lastmod>2026-05-25</lastmod>
     <changefreq>monthly</changefreq>
-    <priority>0.8</priority>
+    <priority>0.9</priority>
+  </url>
+  <url>
+    <loc>https://fastcvpk.online/create</loc>
+    <lastmod>2026-05-25</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.9</priority>
   </url>
   <url>
     <loc>https://fastcvpk.online/blog</loc>
-    <lastmod>2026-05-23</lastmod>
+    <lastmod>2026-05-25</lastmod>
     <changefreq>weekly</changefreq>
     <priority>0.7</priority>
   </url>
   <url>
     <loc>https://fastcvpk.online/blog/cv-kaise-banate-hain</loc>
-    <lastmod>2026-05-23</lastmod>
+    <lastmod>2026-05-25</lastmod>
     <priority>0.6</priority>
   </url>
   <url>
     <loc>https://fastcvpk.online/blog/ats-friendly-cv-kya-hota-hai</loc>
-    <lastmod>2026-05-23</lastmod>
+    <lastmod>2026-05-25</lastmod>
     <priority>0.6</priority>
   </url>
   <url>
     <loc>https://fastcvpk.online/blog/student-cv-guide-pakistan</loc>
-    <lastmod>2026-05-23</lastmod>
+    <lastmod>2026-05-25</lastmod>
     <priority>0.6</priority>
   </url>
   <url>
     <loc>https://fastcvpk.online/blog/biodata-format-pakistan</loc>
-    <lastmod>2026-05-23</lastmod>
+    <lastmod>2026-05-25</lastmod>
     <priority>0.6</priority>
   </url>
   <url>
     <loc>https://fastcvpk.online/blog/freelancer-cv-tips-pakistan</loc>
-    <lastmod>2026-05-23</lastmod>
+    <lastmod>2026-05-25</lastmod>
     <priority>0.6</priority>
   </url>
 </urlset>`;
